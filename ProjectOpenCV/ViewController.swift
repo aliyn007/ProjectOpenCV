cc84d95660d5ac3d7c3c6ea4394a72bd1deb118e
Aleksandr <a.ilyin007@gmail.com>
2022-02-17 14:10:30 +0200

    Initial Commit

---
+++ ProjectOpenCV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectOpenCV
+//
+//  Created by Александр on 17.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
